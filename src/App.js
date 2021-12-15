d4ddee8da9cb42027f17e7fea61852ea7343b5cc
salma chebbi <salmaaa.shebby@gmail.com>
2021-12-15 10:30:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './fessfess/About';
+import Contact from './fessfess/Contact';
+import Footer from './fessfess/Footer';
+import Header from './fessfess/Header';
+import Projects from './fessfess/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Header/>
+  <About/>
+  <Projects/>
+  <Contact/>
+  <Footer/>
+
   );
 }
 
---
+++ src/fessfess/Footer.js
@@ -0,0 +1,10 @@
+
+
+
+function Footer() {
+    return (    <footer>
+        <p>Copyritghts &copy; GOMYCODE 2020</p>
+      </footer>)
+    
+}
+export default Footer
